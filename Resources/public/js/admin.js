d9e5fd4a855cc2debbb3cf435fd3bea279f707f8
mgiagnoni <gimassimo@gmail.com>
2011-07-07 13:14:01 +0200

    Tweaked template for publish/unpublish node actions

--- Resources/public/js/admin.js
+++ Resources/public/js/admin.js
@@ -4,8 +4,8 @@ var configUI = {
         'delete': 'ui-icon-trash',
         'moveup': 'ui-icon-arrowthick-1-n',
         'movedown': 'ui-icon-arrowthick-1-s',
-        'publish': 'ui-icon-circle-check',
-        'unpublish': 'ui-icon-circle-close',
+        'unpublish': 'ui-icon-circle-check',
+        'publish': 'ui-icon-circle-close',
         'back': 'ui-icon-arrowthick-1-w',
         'new': 'ui-icon-document',
         'move': 'ui-icon-arrowthick-2-ne-sw'
